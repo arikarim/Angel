846de96640d2fdf979efd477b85805e6f3b441dc
arikarim <arikarim146@gmail.com>
2021-08-04 21:22:18 +0300

    Add details to user

--- Gemfile
+++ Gemfile
@@ -6,6 +6,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.0.0'
 gem 'devise-jwt', '~> 0.8.1'
 gem 'dotenv-rails'
+
+gem 'image_processing', '~> 1.2'
+gem 'shrine', '~> 3.3'
+gem "shrine-cloudinary", "~> 1.1"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4'
 # Use sqlite3 as the database for Active Record
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,35 +3,15 @@
 class ApplicationController < ActionController::API
   respond_to :json
 
+  protect_from_forgery with: :exception
 
-  # before_action :process_token
+     before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # private
+     protected
 
-  # # Check for auth headers - if present, decode or send unauthorized response (called always to allow current_user)
-  # def process_token
-  #   if request.headers['Authorization'].present?
-  #     begin
-  #       jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.secrets.secret_key_base).first
-  #       @current_user_id = jwt_payload['id']
-  #     rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
-  #       head :unauthorized
-  #     end
-  #   end
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :bio, :about, :available, :password, :image,:password_confirmation)}
 
-  # # If user has not signed in, return unauthorized response (called only when auth is needed)
-  # def authenticate_user!(options = {})
-  #   head :unauthorized unless signed_in?
-  # end
-
-  # # set Devise's current_user using decoded JWT instead of session
-  # # def current_user
-  # #   @current_user ||= super || User.find(@current_user_id)
-  # # end
-
-  # # check that authenticate_user has successfully returned @current_user_id (user is authenticated)
-  # def signed_in?
-  #   @current_user_id.present?
-  # end
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :bio, :about, :available, :image, :password, :current_password,:password_confirmation)}
+  end
 end
